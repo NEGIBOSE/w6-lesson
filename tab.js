52331540a17f677ffabbc5dda03629277dd2108c
NEGIBOSE <ryo0827ngsh@icloud.com>
2023-11-23 18:25:11 +0900

    リファクタリング

--- tab.js
+++ tab.js
@@ -4,6 +4,8 @@
   const $tab = $doc.getElementById("js-tab");
   const $nav = $tab.querySelectorAll("[data-nav]");
   const $content = $tab.querySelectorAll("[data-content]");
+  const ACTIVE_CLASS = "is-active";
+  const navLen = $nav.length;
 
   //初期化
   const init = () => {
@@ -21,9 +23,9 @@
 
     //対象外のnav、contentすべて一旦リセットする
     let index = 0;
-    while (index < $nav.length) {
+    while (index < navLen) {
       $content[index].style.display = "none";
-      $nav[index].classList.remove("is-active");
+      $nav[index].classList.remove(ACTIVE_CLASS);
       index++;
     }
 
@@ -31,12 +33,12 @@
     $tab.querySelectorAll(
       '[data-content="' + targetVal + '"]'
     )[0].style.display = "block";
-    $nav[targetVal].classList.add("is-active");
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
   };
 
   //前nav要素に対して関数を適応・発火
   let index = 0;
-  while (index < $nav.length) {
+  while (index < navLen) {
     $nav[index].addEventListener("click", (e) => handleClick(e));
     index++;
   }
